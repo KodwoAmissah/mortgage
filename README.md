c050738c32efcc69ce4d03f8a8c1a668f7477560
Kodwo Amissah-Mensah <kodwoam@gmail.com>
2023-06-23 09:03:30 +0000

    Removing unncessary values that interfer with the code

---
+++ README.md
@@ -0,0 +1,2 @@
+##Description
+This code allows one to calculate the mortgage loan at a certain year and a t a certain interest rate and the amount of money they will need to pay for the loan
--- mortgage.py
+++ mortgage.py
@@ -10,9 +10,6 @@ Years_to_pay_off_mortgage=int(input("Enter years to pay off mortgage: "))
 Amount_in_months_to_pay_mortgage=Years_to_pay_off_mortgage*12
 Interest_rate_monthly_percent=percent_interest/12
 monthly_payment=mortgage_loan_principal*(Interest_rate_monthly_percent*((1+Interest_rate_monthly_percent)**Amount_in_months_to_pay_mortgage))/(((1+Interest_rate_monthly_percent)**Amount_in_months_to_pay_mortgage)-1)
-print(f"For a {Years_to_pay_off_mortgage}-year mortgage loan of ${mortgage_loan_principal} at annual interest rate of {interest}% you pay ${monthly_payment:.2f}") 
-print()
-print("monthly")
-print()
+print(f"For a {Years_to_pay_off_mortgage}-year mortgage loan of ${mortgage_loan_principal} at annual interest rate of {interest}% you pay ${monthly_payment:.2f} monthly") 
 Total_amount=monthly_payment*Amount_in_months_to_pay_mortgage
 print(f"Total amount paid will be ${Total_amount:.2f}")
